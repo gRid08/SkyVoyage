382a8d38abd536de38ee8dadd19d04392bb734b4
Ashutosh Devanpalli <ashutoshdevanpalli86@gmail.com>
2024-08-16 21:26:47 +0000

    Fixed mongo issue

--- server/db.js
+++ server/db.js
@@ -3,8 +3,8 @@ const { MongoClient } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// Replace these with your MongoDB Atlas connection details
-const uri = 'mongodb+srv://User123:Pass@123@cluster0.1zmwd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+// Replace with your username and password
+const uri = 'mongodb+srv://<username>:<password>@cluster0.1zmwd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
--- server/script.js
+++ server/script.js
@@ -42,7 +42,8 @@ app.get("/api/flights", async (req, res) => {
   }
 });
 
-const uri = 'mongodb+srv://User123:Pass@123@cluster0.1zmwd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+//Enter your username and password
+const uri = 'mongodb+srv://<username>:<password>@cluster0.1zmwd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
